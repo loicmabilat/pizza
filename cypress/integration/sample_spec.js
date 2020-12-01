d1d14cbe734acab949b2e85061a020a8e5cf2f97
loic mabilat <loicbmt@gmail.com>
2020-12-01 10:22:34 +0100

    adresse golive et test get

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,13 @@
 describe('My First Test', () => {
     it('charger le site', ()=>{
-        cy.visit('/pizza.html');
-        
+        cy.visit('http://127.0.0.1:5500/pizza/index.html');
+        cy.contains('type').click()
+
+        cy.url()sould('include','/commands/actions')
+        // get une entre, ecrit a l'interieur et verifie que la valeur a ete mise a jour
+        cy.get('action-email')
+        .type('fake@email.com')
+        .should('have.value','fake@email.com')
+
     })
     })
