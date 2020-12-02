af709fe15d173e874c4908090e792a5f85fd8394
loic mabilat <loicbmt@gmail.com>
2020-12-02 09:47:50 +0100

    index

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,13 +1,6 @@
-describe('My First Test', () => {
-    it('charger le site', ()=>{
-        cy.visit('http://127.0.0.1:5500/pizza/index.html');
-        cy.contains('type').click()
-
-        cy.url()sould('include','/commands/actions')
-        // get une entre, ecrit a l'interieur et verifie que la valeur a ete mise a jour
-        cy.get('action-email')
-        .type('fake@email.com')
-        .should('have.value','fake@email.com')
-
-    })
-    })
+describe('test de mon site pizzalacolle', () => {
+    it('click sur la pizza', () => {
+        it('Does not do much!', () => {
+            expect(true).to.equal(false)
+        })
+        })
